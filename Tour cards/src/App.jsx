a0bf4018ce5cff093c0ce1f0d5bf11ff3ba48e19
Sheetu1 <sheetusharma548@gmail.com>
2025-05-20 00:09:32 +0530

    Done

--- Tour cards/src/App.jsx
+++ Tour cards/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
  if (tours.length === 0) {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-center px-4">
-      <h1 className="text-3xl font-semibold text-gray-700 mb-6">No Tours Left...</h1>
+      <h1 className="text-3xl font-semibold text-gray-700 mb-6">No Tours Left.</h1>
       <button
         onClick={() => setTours(data)}
         className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-600 transition duration-300"
