b235192d759fa4c43c9f79bcacd83f210bc681e8
Sheetu1 <sheetusharma548@gmail.com>
2025-05-30 04:38:16 +0530

    All input have new state

--- Form-Handling/src/App.jsx
+++ Form-Handling/src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  // const [name,setname] = useState(" ")
+  // const [email,setemail] = useState(" ")
+  // function NameHandler (e) {
+  //   console.log(e.target.value);
+    
+  // }
+  // function EmailHandler (e) {
+  //   console.log(e.target.value);
+  // }
   return(
     <div>
-      <h1>App</h1>
+      <form>
+        <input onChange={NameHandler} type="text"  placeholder="Enter Name"/>
+        <input onChange={EmailHandler} type="email" placeholder="Enter Email" />
+        <input type="password" placeholder="Enter Password" />
+      </form>
     </div>
   )
 }
