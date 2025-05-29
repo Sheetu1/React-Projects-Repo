656d5aa468a77d44cfb5fb0dcc340bf9de0c932d
Sheetu1 <sheetusharma548@gmail.com>
2025-05-30 04:54:44 +0530

    Multiple element handle using one handler and ine statae

--- Form-Handling/src/App.jsx
+++ Form-Handling/src/App.jsx
@@ -1,23 +1,52 @@
 import React, { useState } from "react";
 
 const App = () => {
+  // All element have new state
+
   // const [name,setname] = useState(" ")
   // const [email,setemail] = useState(" ")
   // function NameHandler (e) {
   //   console.log(e.target.value);
-    
+
   // }
   // function EmailHandler (e) {
   //   console.log(e.target.value);
   // }
-  return(
+
+  // All Element have one state
+  const [formData, setformdData] = useState({ name: "", email: "" });
+
+  function ChangeHandler(e) {
+    setformdData((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  return (
     <div>
       <form>
-        <input onChange={NameHandler} type="text"  placeholder="Enter Name"/>
-        <input onChange={EmailHandler} type="email" placeholder="Enter Email" />
-        <input type="password" placeholder="Enter Password" />
+        {/* <input onChange={NameHandler} type="text"  placeholder="Enter Name"/>
+        <input onChange={EmailHandler} type="email" placeholder="Enter Email" /> */}
+
+        <input
+          onChange={ChangeHandler}
+          type="text"
+          placeholder="Enter Name"
+          name="Name"
+          value={formData.name}
+        />
+        <input
+          onChange={ChangeHandler}
+          type="email"
+          placeholder="Enter Email"
+          name="Email"
+          value={formData.email}
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 export default App;
