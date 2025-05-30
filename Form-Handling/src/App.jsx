5f424eb4bcd60c84674cdae1f3fe86d1cd2fdfb5
Sheetu1 <sheetusharma548@gmail.com>
2025-05-30 06:48:28 +0530

    checkboxes and radio btn how to work

--- Form-Handling/src/App.jsx
+++ Form-Handling/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
   // All element have new state
@@ -14,13 +15,25 @@ const App = () => {
   // }
 
   // All Element have one state
-  const [formData, setformdData] = useState({ name: "", email: "" });
+  const [formData, setformdData] = useState({
+    name: "",
+    email: "",
+    comments: "",
+    isVisible: true,
+    mode: "",
+  });
+  console.log(formData);
+
+  //  useEffect(() => {
+  //   console.log("formData updated:", formData); // ✅ updated values
+  // }, [formData]);
 
   function ChangeHandler(e) {
-    setformdData((prevData) => {
+    const { name, value, type, checked } = e.target;
+    setformdData((prevFormData) => {
       return {
-        ...prevData,
-        [e.target.name]: e.target.value,
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
       };
     });
   }
@@ -30,21 +43,62 @@ const App = () => {
       <form>
         {/* <input onChange={NameHandler} type="text"  placeholder="Enter Name"/>
         <input onChange={EmailHandler} type="email" placeholder="Enter Email" /> */}
-
+        <br />
         <input
           onChange={ChangeHandler}
           type="text"
           placeholder="Enter Name"
-          name="Name"
+          name="name"
           value={formData.name}
         />
+        <br />
+        <br />
         <input
           onChange={ChangeHandler}
           type="email"
           placeholder="Enter Email"
-          name="Email"
+          name="email"
           value={formData.email}
         />
+        <br />
+        <br />
+        <textarea
+          onChange={ChangeHandler}
+          name="comments"
+          placeholder="Enter Comments"
+        ></textarea>
+        <br />
+        <br />
+        <input
+          onChange={ChangeHandler}
+          type="checkbox"
+          name="isVisible"
+          id="isVisible"
+          checked={formData.isVisible}
+        />
+        <label htmlFor="isVisible">I am visible</label>
+        <br />
+        <br />
+        <input
+          onChange={ChangeHandler}
+          type="radio"
+          id="onlinemode"
+          value="onlinemode"
+          name="mode"
+          checked={formData.mode === "onlinemode"}
+          />
+        <label htmlFor="mode">Online mode</label>
+         <br />
+        <br />
+        <input
+          onChange={ChangeHandler}
+          type="radio"
+          id="offlinemode"
+          value="offlinemode"
+          name="mode"
+          checked={formData.mode === "offlinemode"}        
+          />
+        <label htmlFor="mode">Offline mode</label>
       </form>
     </div>
   );
