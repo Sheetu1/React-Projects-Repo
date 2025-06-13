9b65d0112ae205e0f209ff3a709a448fe31d09c9
Sheetu1 <sheetusharma548@gmail.com>
2025-06-13 08:32:35 +0530

    Add Task done

--- ToDo-List/src/App.jsx
+++ ToDo-List/src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [Todo, setTodo] = useState('');
+  const [Todos, setTodos] = useState([]);
 
-export default App
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(Todo);
+    setTodos([...Todos, Todo]);
+    setTodo("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          value={Todo}
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          placeholder="Enter Task"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+
+      {/* ✅ Correct conditional rendering */}
+      {
+        Todos.length > 0 ? (
+          Todos.map((item, idx) => (
+            <div key={idx}>
+              <h1>{item}</h1>
+            </div>
+
+          ))
+        ) : null
+      }
+    </div>
+  );
+};
+
+export default App;
